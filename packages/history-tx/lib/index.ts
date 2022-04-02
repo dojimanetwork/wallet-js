5ad6436d380f0b8a1e1bc9ca5cdb12ed357c5305
bhaagiKenpachi <bhagath.reddy@dojima.network>
2022-04-02 21:12:55 +0530

    dev: pub: history-tx and removed http util

--- packages/history-tx/lib/index.ts
+++ packages/history-tx/lib/index.ts
@@ -1,14 +1,5 @@
-import { http, get, put, post } from "./utils/http";
 import BitcoinTransactions from "./bitcoin";
 import EthereumTransactions from "./ethereum";
 import SolanaTransactions from "./solana";
 
-export {
-  http,
-  get,
-  put,
-  post,
-  BitcoinTransactions,
-  EthereumTransactions,
-  SolanaTransactions,
-};
+export { BitcoinTransactions, EthereumTransactions, SolanaTransactions };
--- packages/history-tx/lib/polkadot.ts
+++ packages/history-tx/lib/polkadot.ts
@@ -1,27 +1,24 @@
-import { post } from "./index";
 export default async function getPolka() {
-  var rawBody = JSON.stringify({
-    row: 10,
-    page: 1,
-    address: "5FKpdt1vdgQ2CNeNQXN9evodm4wBH6655R1GyV44tBdNgXP6",
-  });
-
-  //   var requestOptions = {
-  //     method: "POST",
+  // var rawBody = JSON.stringify({
+  //   row: 10,
+  //   page: 1,
+  //   address: "5FKpdt1vdgQ2CNeNQXN9evodm4wBH6655R1GyV44tBdNgXP6",
+  // });
+  // //   var requestOptions = {
+  // //     method: "POST",
+  // //     body: rawBody,
+  // //     redirect: "follow",
+  // //   };
+  // var response = await post(
+  //   "https://westend.api.subscan.io/api/scan/transfers",
+  //   {
+  //     method: "post",
   //     body: rawBody,
   //     redirect: "follow",
-  //   };
-
-  var response = await post(
-    "https://westend.api.subscan.io/api/scan/transfers",
-    {
-      method: "post",
-      body: rawBody,
-      redirect: "follow",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
-  console.log(response);
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   }
+  // );
+  // console.log(response);
 }
