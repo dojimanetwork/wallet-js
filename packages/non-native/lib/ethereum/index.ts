b7fe478c9416f828b84c84cedea163cd429a1d32
yerramreddyuday <uday.prasad@dojima.network>
2022-04-11 18:56:31 +0530

    dev: mod: Added new exports in non-native package

--- packages/non-native/lib/ethereum/index.ts
+++ packages/non-native/lib/ethereum/index.ts
@@ -1,3 +1,11 @@
 import Erc20TokenBalance from "./erc20TokensBalance";
+import {
+  contractData,
+  tokenList,
+  tokenSymbolList,
+  minABI,
+  BalanceResult,
+} from "./utils";
 
+export { contractData, tokenList, tokenSymbolList, minABI, BalanceResult };
 export default Erc20TokenBalance;
