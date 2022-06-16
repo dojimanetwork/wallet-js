763be974e85c2e8b69135e7c2c647bc505ad2712
yerramreddyuday <uday.prasad@dojima.network>
2022-06-16 12:07:38 +0530

    dev: mod: Modified imports

--- packages/connection/lib/index.ts
+++ packages/connection/lib/index.ts
@@ -1,6 +1,6 @@
 import ArweaveInitialise from "./arweaveInit";
-import BitcoinClient from "./bitcoinClient";
+import BtcClient from "./bitcoinClient";
 import EthereumWeb3 from "./ethereumWeb3";
 import SolanaConnection from "./solanaConnection";
 
-export { ArweaveInitialise, BitcoinClient, EthereumWeb3, SolanaConnection };
+export { ArweaveInitialise, BtcClient, EthereumWeb3, SolanaConnection };
