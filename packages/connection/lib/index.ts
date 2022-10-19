8a4342fa18f66045ae6e24fb50b4d3bd1dd9febf
yerramreddyuday <uday.prasad@dojima.network>
2022-10-19 22:32:00 +0530

    dev: add: Binance client in connection package

--- packages/connection/lib/index.ts
+++ packages/connection/lib/index.ts
@@ -1,6 +1,13 @@
 import ArweaveInitialise from "./arweaveInit";
+import BinanceClient from "./binanceClient";
 import BtcClient from "./bitcoinClient";
 import EthereumWeb3 from "./ethereumWeb3";
 import SolanaConnection from "./solanaConnection";
 
-export { ArweaveInitialise, BtcClient, EthereumWeb3, SolanaConnection };
+export {
+  ArweaveInitialise,
+  BinanceClient,
+  BtcClient,
+  EthereumWeb3,
+  SolanaConnection,
+};
