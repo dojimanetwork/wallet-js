e054896df339411fcb30bc8424fce6a543f174f8
ritwik-dojima <ritwik.reddy@dojima.network>
2022-10-27 12:19:07 +0530

    add Binance transfer function

--- packages/transfer/lib/index.ts
+++ packages/transfer/lib/index.ts
@@ -2,7 +2,8 @@ import ArweaveChain from "./chains/arweave";
 import BitcoinChain from "./chains/bitcoin";
 import EthereumChain from "./chains/ethereum";
 import SolanaChain from "./chains/solana";
+import BinanceChain from "./chains/binance";
 import { GasfeeResult } from "./chains/utils";
 
 export { GasfeeResult };
-export { SolanaChain, ArweaveChain, EthereumChain, BitcoinChain };
+export { BinanceChain, SolanaChain, ArweaveChain, EthereumChain, BitcoinChain };
