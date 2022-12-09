dca55fc9d31c296c5494fed25e656406d61919aa
yerramreddyuday <uday.prasad@dojima.network>
2022-12-09 09:50:55 +0530

    dev: add: Modified utils package

--- packages/utils/lib/index.ts
+++ packages/utils/lib/index.ts
@@ -1,7 +1,15 @@
-import {
-  convertDateToTimestamp,
-  convertISOtoUTC,
-  convertTimestampToDate,
-} from "./moment";
-
-export { convertDateToTimestamp, convertISOtoUTC, convertTimestampToDate };
+export * from "./async";
+export * from "./asset";
+export {
+  default as bn,
+  isValidBN,
+  bnOrZero,
+  validBNOrZero,
+  formatBN,
+  formatBNCurrency,
+  fixedBN,
+} from "./bn";
+export * from "./chain";
+export * from "./string";
+export * from "./types";
+export * from "./moment";
