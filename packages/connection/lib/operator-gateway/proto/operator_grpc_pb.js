0fcaa2def453b4784c3ea00ede836de344b2cd3a
Aryan Tikarya <aryan.tikarya@dojima.network>
2025-01-09 09:52:47 +0530

    small fixes

--- packages/connection/lib/operator-gateway/proto/operator_grpc_pb.js
+++ packages/connection/lib/operator-gateway/proto/operator_grpc_pb.js
@@ -1,7 +1,7 @@
 // GENERATED CODE -- DO NOT EDIT!
 
 "use strict";
-var grpc = require("grpc");
+var grpc = require("@grpc/grpc-js");
 var operator_pb = require("./operator_pb.js");
 var query_pb = require("./query_pb.js");
 
